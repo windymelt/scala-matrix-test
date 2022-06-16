48a036bcb8b92404109467aa36fbfe763064cab9
Windymelt <windymelt@3qe.us>
2022-06-17 01:43:36 +0900

    break apart tests

---
+++ src/test/scala/example/No3Spec.scala
@@ -0,0 +1,11 @@
+package example
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class No3Spec extends AnyFlatSpec with Matchers {
+  "test" should "do sleep and pass" in {
+    Thread.sleep(10000)
+    true shouldBe true
+  }
+}
